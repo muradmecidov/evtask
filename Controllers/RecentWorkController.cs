f674451ed43e06392d37441de6e6b84089f56117
NabiyevSaleh <saleh.nabiyev@mail.ru>
2023-05-18 16:52:58 +0400

    evtask

---
+++ Controllers/RecentWorkController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebFrontToBack.DAL;
+using WebFrontToBack.Models;
+using WebFrontToBack.ViewModel;
+
+
+namespace WebFrontToBack.Controllers
+{
+    public class RecentWorkController : Controller
+    {
+        protected readonly AppDbContext _context;
+
+        public RecentWorkController(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View( await _context.RecentWorks.ToListAsync());
+        }
+    }
+}
